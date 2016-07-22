1b7b1590e0d838eec3e828e6aa45d77366c7e395
root <tsunomur@microsoft.com>
2016-07-22 20:28:30 +0900

    Modify

--- IsVmInMaintforCron.sh
+++ IsVmInMaintforCron.sh
@@ -8,8 +8,8 @@ SCRIPT_NAME=$(basename $0)
 RESULT1_FILE="$CWD/result1.out"
 RESULT2_FILE="$CWD/result2.out"
 LOGGER="logger -is -t $SCRIPT_NAME"
-CHECK_URL="http://169.254.169.254/metadata/v1/maintenance"
-#CHECK_URL="http://localhost/testazure.html"
+#CHECK_URL="http://169.254.169.254/metadata/v1/maintenance"
+CHECK_URL="http://azure-nomupro.cloudapp.net:8080/testazure.html"
 RETVAL=0
 
 # 結果ファイルの作成
@@ -21,12 +21,17 @@ IS_EVENT=$?
 
 # 前回と今回のファイルを比較
 diff -q $RESULT1_FILE $RESULT2_FILE
-# 差分があり、かつ、イベントが発生している時
-# (イベント状態からイベント無し状態の遷移を考慮)
-if [ $? -gt 0 -a $IS_EVENT -eq 0 ]; then
-    $LOGGER "Incoming VM Event"
+IS_DIFF=$?
+# 差分があり、かつ、イベントが発生している時にlogger出力
+# - イベント状態からイベント無し状態の遷移の時はloggerにも出さず、戻り値も0のまま
+# - イベント状態からイベント状態の遷移の時は、logger に出力せず、戻り値は1
+if [ $IS_EVENT -eq 0 ]; then
+    if [ $IS_DIFF -gt 0 ]; then
+	$LOGGER "Incoming VM Event" - $(cat $RESULT1_FILE)
+    fi
     RETVAL=1
 fi
 mv $RESULT1_FILE $RESULT2_FILE
 
+echo "---$RETVAL---"
 exit $RETVAL
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # IsVmInMaintforCron
+
+## 実行方法
+    ./IsVmInMaintforCron.sh
+
+## Syslog 出力例
+    Jul 22 20:25:54 alinsrv01 IsVmInMaintforCron.sh[61314]: Incoming VM Event - "EventID": "1a0a13a3-dc0d-4bbe-ab24-df710a3917e6",
+    Jul 22 20:26:24 alinsrv01 IsVmInMaintforCron.sh[61526]: Incoming VM Event - "EventID": "1a0a13a3-dc0d-4bbe-ab24-df710a3917e6",
+    Jul 22 20:26:33 alinsrv01 IsVmInMaintforCron.sh[61564]: Incoming VM Event - "EventID": "0a0a13a3-dc0d-4bbe-ab24-df710a3917e6",
+
+## 戻り値
+    イベントがある場合 常に1 を返します。
+
+## パターン
+    * イベント無し->イベント発生 = logger 出力 + 戻り値 1
+    * イベント発生->イベント発生 = **logger 出力無** + 戻り値 1
+    * イベント発生->イベント無し = logger 出力無し + 戻り値 0
+    * イベント発生->イベント発生(EventID違い) = logger 出力 + 戻り値 1
+
+## 備考
+EventID の出力の仕方次第では、うまくloggerにEventIDは出力され無いかもしれません(EventIDと:の間に改行がある等)。
